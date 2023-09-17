e13a129e18f68c6dbe184b884e0da0f65314660e
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-17 19:55:05 +0100

    initializing folders

---
+++ 5kyu/domain name/js/main.js
@@ -0,0 +1,5 @@
+//Extract domain name from URL
+//Write a function which when given a url as a string, parses out just the domain name as a string
+//'http://www.github.com/hoyw/' ==> 'github'
+//'http://github.com/hoyw/' ==> 'github'
+
