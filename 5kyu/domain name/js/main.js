3462ec209011ca0b229a8226b60830cd9b11a9de
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-17 20:46:16 +0100

    created function to get domain name

--- 5kyu/domain name/js/main.js
+++ 5kyu/domain name/js/main.js
@@ -2,4 +2,20 @@
 //Write a function which when given a url as a string, parses out just the domain name as a string
 //'http://www.github.com/hoyw/' ==> 'github'
 //'http://github.com/hoyw/' ==> 'github'
+//'http://github.com/hoyw/' ==> 'github'
 
+function produceDomain(str) {
+  slashString = str.split("/");
+  // 0: "http:"
+  // 1: ""
+  // 2: "www.github.com"
+  // 3: "hoyw"
+  // 4: ""
+  domainArray = slashString
+    .filter((item) => item.includes(".com"))
+    .map((item) => item.split("."));
+  return domainArray[0][domainArray[0].length - 2];
+}
+
+let string = "http://www.github.com/hoyw/";
+console.log(produceDomain(string));
