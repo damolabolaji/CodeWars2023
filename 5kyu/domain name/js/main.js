cef6203b68cdecc3794f1b20beb2947bf2c24a83
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-18 23:34:16 +0100

    testing with different domain names. failed

--- 5kyu/domain name/js/main.js
+++ 5kyu/domain name/js/main.js
@@ -6,16 +6,16 @@
 
 function produceDomain(str) {
   slashString = str.split("/");
-  // 0: "http:"
-  // 1: ""
-  // 2: "www.github.com"
-  // 3: "hoyw"
-  // 4: ""
+
   domainArray = slashString
     .filter((item) => item.includes(".com"))
     .map((item) => item.split("."));
-  return domainArray[0][domainArray[0].length - 2];
+  if (domainArray[0][1] != "com") {
+    return domainArray[0][1];
+  } else return domainArray[0][0];
+
+  // domainArray[0][domainArray[0].length - 2];
 }
 
-let string = "http://www.github.com";
+let string = "https://youtube.com";
 console.log(produceDomain(string));
