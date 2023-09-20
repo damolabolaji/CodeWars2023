90852eafc3f5a5b514725e9fe1371dd75ef97113
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-20 22:09:21 +0100

    redo: codewars solution

--- 5kyu/domain name/js/main.js
+++ 5kyu/domain name/js/main.js
@@ -4,26 +4,33 @@
 //'http://github.com/hoyw/' ==> 'github'
 //'http://github.com/hoyw/' ==> 'github'
 
-function produceDomain(str) {
-  slashString = str.split("/");
+// function produceDomain(str) {
+//   slashString = str.split("/");
 
-  domainArray = slashString
-    .filter((item) => item.includes(".com"))
-    .map((item) => item.split("."));
-  if (domainArray[0][1] != "com") {
-    return domainArray[0][1];
-  } else return domainArray[0][0];
+//   domainArray = slashString
+//     .filter((item) => item.includes(".com"))
+//     .map((item) => item.split("."));
+//   if (domainArray[0][1] != "com") {
+//     return domainArray[0][1];
+//   } else return domainArray[0][0];
 
-  // domainArray[0][domainArray[0].length - 2];
-}
+//   // domainArray[0][domainArray[0].length - 2];
+// }
 
-let string = "https://youtube.com";
-console.log(produceDomain(string));
+let string = "https://youtube.jp.com";
+// console.log(produceDomain(string));
 
 //codewars solution
-function domainName(url){
-  url = url.replace("https://", '');
-  url = url.replace("http://", '');
-  url = url.replace("www.", '');
-  return url.split('.')[0];
-};
+function domainName(url) {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www", "")
+    .split(".")[0];
+  //   url = url.replace("https://", '');
+  //   url = url.replace("http://", '');
+  //   url = url.replace("www.", '');
+  //   return url.split('.')[0];
+}
+
+console.log(domainName(string));
