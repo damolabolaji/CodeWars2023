48dd124976003572a0d6fa5fb17412ec3b42fb6a
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-23 22:59:02 +0100

    Pangram correct solution using for-of loop

--- 6kyu/Pangram/js/main.js
+++ 6kyu/Pangram/js/main.js
@@ -9,16 +9,29 @@
 //fromCharCode(i) loops through character codes numbers
 //a string of all alphabets should be equal to all aphabets of the given string
 
-function checkLetters(str) {
-  check = /^[A-Za-z]+$/;
-  if (str.match(check).length === 26) {
-    return true;
-  } else return false;
+//DID NOT WORK. NEEDS REDO
+// function checkLetters(str) {
+//   check = /^[A-Za-z]+$/;
+//   if (str.match(check).length === 26) {
+//     return true;
+//   } else return false;
+// }
+// let check = /[A-Za-z]/g;
+const string = "The quick brown fox jump over the lazy dog";
+
+// console.log(string.toLowerCase().match(check));
+
+//CORRECT SOLUTION
+function checkAlphabeth(str) {
+  check = "abcdefghijklmnopqrstuvwxyz";
+  LowerStr = str.toLowerCase();
+  for (letter of check) {
+    if (!LowerStr.includes(letter)) {
+      //   console.log(letter);
+      return false;
+    }
+  }
+  return true;
 }
-let check = /[A-Za-z]/g;
-const string = "The quick brown fox jumps over the lazy dog";
 
-console.log(string.toLowerCase().match(check));
-
-
-
+console.log(checkAlphabeth(string));
