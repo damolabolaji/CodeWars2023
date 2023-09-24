56a64fb05f7b2b4a0075e3dbb458f5a5aefff133
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-24 14:19:49 +0100

    added first solution to Not Secure

--- 5kyu/Not Secure/js/main.js
+++ 5kyu/Not Secure/js/main.js
@@ -6,3 +6,19 @@
 // At least one character ("" is not valid)
 // Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
 // No whitespaces / underscore
+
+let string = "";
+const check = /^[A-Za-z0-9]*$/;
+
+function checkAlphanumeric(str) {
+  if (str === "") {
+    return false;
+  } else if (str.match(check)) {
+    //produces an array of the matches or null if nothing matches
+    return true;
+  }
+  return false;
+}
+
+console.log(checkAlphanumeric(string));
+//console.log(string.match(check)); //produces null. if just one value doesnt match, the whole thing produces null
