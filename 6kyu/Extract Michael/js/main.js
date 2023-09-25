84e4f27a2b0c36f13f96bb4678920c358b2bbab9
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-25 22:32:56 +0100

    added pseudo code for Extract Michael

--- 6kyu/Extract Michael/js/main.js
+++ 6kyu/Extract Michael/js/main.js
@@ -2,3 +2,7 @@
 //e.g Michael, how is Michael Richards. did you see Michael Reeds today
 //Output will be [Richards,Reeds]
 
+function extractMichael(str) {
+  //get the text to split into an array
+  //if you see a michael without a a comma after it, take the next element after it and put it in an array.
+}
