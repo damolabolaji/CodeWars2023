f342b5efc247342a59936623078ea07a55592991
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-26 23:51:45 +0100

    First attempt Extract Michael

--- 6kyu/Extract Michael/js/main.js
+++ 6kyu/Extract Michael/js/main.js
@@ -2,7 +2,19 @@
 //e.g Michael, how is Michael Richards. did you see Michael Reeds today
 //Output will be [Richards,Reeds]
 
+const string =
+  "Michael, how is Michael Richards. did you see Michael Reeds today";
 function extractMichael(str) {
-  //get the text to split into an array
+  //crete an empty array to store the values
+  surnames = [];
+  //get the text to split into an array using spaces
+  splitStri = str.split(" ");
   //if you see a michael without a a comma after it, take the next element after it and put it in an array.
+  for (let names of splitStri) {
+    if ("Michael") {
+      surnames.push(names);
+    }
+  }
+  return surnames;
 }
+console.log(extractMichael(string));
