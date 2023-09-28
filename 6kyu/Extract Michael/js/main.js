19e117e0e71444d07566958f86f19e5b9b2113e8
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-28 22:49:07 +0100

    testing solution for Extraxt Michael

--- 6kyu/Extract Michael/js/main.js
+++ 6kyu/Extract Michael/js/main.js
@@ -3,12 +3,15 @@
 //Output will be [Richards,Reeds]
 
 const string =
-  "Michael, how is Michael Richards. did you see Michael Reeds today? i saw Michael Cummings esrlier. if Michael Soarez is there, kill him";
+  "Michael, how is Michael Richards. did you see Michael Reeds today? i saw Michael Cummings. esrlier. if Michael Soarez is there, kill him Michael Jordan?";
 function extractMichael(str) {
   //crete an empty array to store the values
   surnames = [];
   //get the text to split into an array using spaces
-  splitStri = str.split(" ");
+  splitStri = str.split(" ").map((item) => {
+    item.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    return item;
+  });
   //if you see a michael without a a comma after it, take the next element after it and put it in an array.
   for (i = 0; i < splitStri.length; i++) {
     if (splitStri[i] === "Michael") {
