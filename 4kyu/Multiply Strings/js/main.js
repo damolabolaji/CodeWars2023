adb184099587e534d8432293c087b3bc161e90ad
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-02 21:39:44 +0100

    added solution for adding in Multiply Strings

--- 4kyu/Multiply Strings/js/main.js
+++ 4kyu/Multiply Strings/js/main.js
@@ -13,4 +13,45 @@ function multiply(a, b) {
   return multiplicatn.toString();
 }
 
-console.log(multiply("34", "34"));
+// In order to perform operations on larger numbers, we convert them into strings and then perform operations on them.
+
+//MEDIUM SOLUTION WALKTHROUGH FOR ADDITION
+//find the digit that is greater in length and let that be the first
+
+function multiplyThisStrings(a, b) {
+  if (a.length >= b.length) {
+    console.log(findSum(a, b));
+  } else findSum(b, a);
+}
+
+var first = "893427328497983427893248932498034289324";
+var second = "234859234879342897893427893274";
+
+if (first.length >= second.length) {
+  console.log(findSum(first, second));
+} else {
+  findSum(second, first);
+}
+
+function findSum(first, second) {
+  var sum = "";
+  var carry = 0;
+  var diff = second.length - first.length;
+  for (i = first.length - 1; i >= 0; i--) {
+    var temp =
+      (Number(first.charAt(i)) % 10) +
+      (Number(second.charAt(i + diff)) % 10) +
+      carry;
+    if (temp >= 10) {
+      sum = (temp % 10) + sum;
+      carry = Math.floor(temp / 10);
+    } else {
+      sum = temp + sum;
+      carry = 0;
+    }
+  }
+  if (carry) {
+    sum = carry + sum;
+  }
+  return sum;
+}
