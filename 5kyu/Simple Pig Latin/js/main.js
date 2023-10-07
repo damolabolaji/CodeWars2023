4b1c321049daad6661e764923dac0335a0e079d6
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-07 01:11:15 +0100

    solution for Simpl Pig Latin

--- 5kyu/Simple Pig Latin/js/main.js
+++ 5kyu/Simple Pig Latin/js/main.js
@@ -1,41 +1,19 @@
-// This problem takes its name by arguably the most important event in the life of the ancient historian Josephus:
-//according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
-// Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and
-//proceeded to kill one man every three, until one last man was left(and that it was supposed to kill himself to end the act).
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
 
-// Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly
-//guessed that they didn't exactly follow through the original idea.
-
-// You are now to create a function that returns a Josephus permutation, taking as parameters the initial
-//array / list of items to be permuted as if they were in a circle and counted out every k places until none remained.
-
-// Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0 to n-1; k will always be >=1.
-
-//for k=3
-// [1,2,3,4,5,6,7] - initial sequence
-// [1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
-// [1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
-// [1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
-// [1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
-// [1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
-// [4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
-// [] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
-
-//[3,6,2,7,5,1,4] is the result
-let array = [1, 2, 3, 4, 5, 6, 7];
-
-function circleArray(arr, k) {
-  newArray = [];
-  let index = 0;
-  for (j = arr.length; j > 0; j--) {
-    n = arr.length;
-    index = (index + k - 1) % n;
-    let item = arr.splice(index, 1);
-    newArray.push(...item);
-  }
-  return newArray;
+function pigIt(str) {
+  //Code here
+  let splitString = str.split(" ");
+  let check = /^[a-zA-Z]+$/;
+  mappedString = splitString.map((item) =>
+    check.test(item)
+      ? item.split("").slice(1).concat(item[0], "ay").join("")
+      : item
+  );
+  return mappedString.join(" ");
 }
 
-console.log(circleArray(array, 3));
-console.log(array);
+console.log(pigIt("Hello World ?!"));
