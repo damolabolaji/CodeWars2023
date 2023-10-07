00ecc230032434727d665a61fcdd4479ea685e8c
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-07 01:39:11 +0100

    added notes in Simple Pig Latin

--- 5kyu/Simple Pig Latin/js/main.js
+++ 5kyu/Simple Pig Latin/js/main.js
@@ -17,3 +17,11 @@ function pigIt(str) {
 }
 
 console.log(pigIt("Hello World ?!"));
+
+//just sampling match()
+let check = /[a-zA-Z]/g;
+//The 'g' flag stands for "global" and is used to specify that a regular expression should perform a global search. 
+//This means that the regex engine will search for all occurrences of the pattern in the input string, rather than stopping after the first match.
+const string = "the quick fox jumps over the lazy dog";
+let matcher = string.match(check);
+console.log(matcher);
