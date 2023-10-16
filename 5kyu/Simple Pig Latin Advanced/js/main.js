e475a0c2fd1b3a5b4778fcedbdec13fa95763154
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-16 23:16:33 +0100

    added pseudo code for Pig Latin Adv

--- 5kyu/Simple Pig Latin Advanced/js/main.js
+++ 5kyu/Simple Pig Latin Advanced/js/main.js
@@ -17,3 +17,19 @@
 // A word will never be a combination of numbers and letters.
 //     Also, there will never be punctuation at the beginning of a word and the
 //     only capital letter in a word will be the first letter meaning there are zero all capitalized words.
+
+//PSEUDO CODE
+//split the word.
+//check that the word is a word
+//take the first letter, change it to lowercase add it to a sliced part of the word and then add 'ay'
+//if the first letter is uppercase, after adding 'ay', change the first letter to uppercase
+//keep taking the letter and and adding it except the letter equals a,e,i,o, or u
+//if the first letter is either a,e,i,o, or u, add 'way' to the back of it
+
+function pigItAdvanced(str) {
+  let mappedString = str.split(" ").map((item) => {
+    if (typeof item === "string") {
+      item.split("").splice(1).concat(item[0], "ay");
+    }
+  });
+}
