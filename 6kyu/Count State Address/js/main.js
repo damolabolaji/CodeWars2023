92ef419c3eb631c2c3d262eff033452c84ca2d24
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-22 01:43:15 +0100

    Added Codewars solution

--- 6kyu/Count State Address/js/main.js
+++ 6kyu/Count State Address/js/main.js
@@ -66,11 +66,47 @@ const myArray = [
 //check the states. for every similar state value, add 1 to the count
 
 function countStates(array) {
-  let count = 0;
-  let newArray = array.reduce((element, obj) => {});
-
+  let newArray = [];
+  array.forEach((element) => {
+    for (let key in element) {
+      if (key === "state") {
+        //add the key=>value pair to an array
+        newArray.push(element[key]);
+      }
+    }
+  });
+  // let objArray = []
+  // objArray.push({
+  //     state:newArray[]
+  // })
   return newArray;
 }
 
-console.log(myArray[1].state);
 console.log(countStates(myArray));
+
+// let arrayInstances = shuffleMe(theArray).reduce((obj, item) => {
+//   if (!obj[item]) {
+//     obj[item] = 0;
+//   }
+//   obj[item]++;
+//   return obj;
+// }, {});
+
+const count = (addres) => {
+  const states = addres.reduce((obj, { state }) => {
+    if (!state) throw new Error();
+    obj[state] = (obj[state] || 0) + 1;
+    return obj;
+  }, {});
+  return Object.keys(states).map((state) => ({ state, count: states[state] }));
+};
+
+function countStt(addresses) {
+  var states = addresses.reduce(function (states, { state }) {
+    if (!state) throw new Error();
+    if (!states[state]) states[state] = 0;
+    return states[state]++, states;
+  }, {});
+
+  return Object.keys(states).map((state) => ({ state, count: states[state] }));
+}
