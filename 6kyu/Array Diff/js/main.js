ed45646423dae28e9428b4eb55415bb2ba984319
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-25 23:50:51 +0100

    solved Array Diff

--- 6kyu/Array Diff/js/main.js
+++ 6kyu/Array Diff/js/main.js
@@ -9,12 +9,15 @@
 
 function arrayDiff(a, b) {
   for (let i = 0; i < a.length; i++) {
-    if (a[i] == b[0]) {
-      a.splice(i, 1);
-      i--;
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] == b[j]) {
+        a.splice(i, 1);
+        i--;
+      }
     }
   }
   return a;
 }
 
-console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+console.log(arrayDiff([1, 2, 2, 2, 3], [2, 3]));
+
