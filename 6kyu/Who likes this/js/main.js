fbbdbecf72d746d81a5c1f415a2cc7f187ee079d
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-28 22:22:29 +0100

    added 2nd solution for Who Likes This

--- 6kyu/Who likes this/js/main.js
+++ 6kyu/Who likes this/js/main.js
@@ -14,7 +14,9 @@ function whoLikesThis(names) {
     if (names.length >= 2) {
       if (names.length >= 3) {
         if (names.length > 3) {
-          return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+          return `${names[0]}, ${names[1]} and ${
+            names.length - 2
+          } others like this`;
         }
         return `${names[0]}, ${names[1]} and ${names[2]} like this`;
       }
@@ -27,3 +29,31 @@ function whoLikesThis(names) {
 }
 
 console.log(whoLikesThis(["peter", "Alex", "Jacob", "Mark", "Max"]));
+
+//SOLUTION TWO
+function likes(names) {
+  names = names || [];
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return names[0] + " likes this";
+      break;
+    case 2:
+      return names[0] + " and " + names[1] + " like this";
+      break;
+    case 3:
+      return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+      break;
+    default:
+      return (
+        names[0] +
+        ", " +
+        names[1] +
+        " and " +
+        (names.length - 2) +
+        " others like this"
+      );
+  }
+}
